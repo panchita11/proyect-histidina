c35d8e8fc064937eafe71a6cf142b7d3684a81d7
panchita11 <teran110805@gmail.com>
2024-11-19 13:48:13 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit as st
 
 st.title("HISTIDINA")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).")
 pip install dash pandas plotly
 
