31dc5d9fbf5abcdb243507f8871d598cc34761ca
panchita11 <teran110805@gmail.com>
2024-11-19 14:08:43 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,8 @@ if opcion == "Información básica":
     varias funciones biológicas. Su fórmula química es **C₆H₉N₃O₂** y es conocida 
     por ser precursora de la histamina.
     """)
+    st.write("""
+   La histidina es uno de los 22 aminoácidos proteinogénicos, aquellos que codifican nuestro código genético y son utilizados para la síntesis de proteínas. Además es considerado uno de los 9 aminoácidos esenciales que requerimos consumir a través de la dieta para evitar sufrir signos de degradación proteica y malnutrición.""")
     st.image(
         "https://thumbs.dreamstime.com/b/f%C3%B3rmula-qu%C3%ADmica-de-histidina-estructura-molecular-ilustraci%C3%B3n-vectorial-aislada-en-fondo-blanco-238646983.jpg",
         caption="Estructura molecular de la histidina",
